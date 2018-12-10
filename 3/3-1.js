f07be476118c9bcea3c6bf2990900abccb00eca7
xiaobaiha <xiaobaihaha0001@163.com>
2018-12-10 21:38:45 +0800

    feat: 3

--- 3/3-1.js
+++ 3/3-1.js
@@ -3,5 +3,24 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    
+  let max = 0;
+  for (let i = 0; i < s.length; i++) {
+    let tempStr = "";
+    let innerIndex = 0;
+    while (
+      i + innerIndex < s.length &&
+      tempStr.indexOf(s[i + innerIndex]) === -1
+    ) {
+      innerIndex++;
+      tempStr = s.slice(i, i + innerIndex);
+      if (innerIndex > max) {
+        max = innerIndex;
+      }
+    }
+  }
+  return max;
 };
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("bbbbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
